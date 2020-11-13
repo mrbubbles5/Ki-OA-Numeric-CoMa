10f2cd8aca2276125e155b98d1608a03a5ce4366
mrbubbles5 <74305101+mrbubbles5@users.noreply.github.com>
2020-11-13 12:14:30 +0100

    Update WS2020_PA01.py

--- CoMa/WS2020_PA01.py
+++ CoMa/WS2020_PA01.py
@@ -1,11 +1,13 @@
 #CoMa1 WS2020
 #f(x) = ax² + bx + c
 #g(x) = dx² + ex + f
+#h(x) = x^5
+#h+g*f = c_5*x^5 + c_4*x^4 + c_3*x^3 + c_2*x^2 + c_1*x^1 + c_0*x^0
 def roots(a=int,b=int,c=int,d=int,e=int,f=int):
     #the function calculates the coefficients, puts them into a list and
     #counts if there is a switch between positive and negative Elements
     #if a switch mod 2 = 0 is found, the function will return the given
-    #message aboout the squirts
+    #message aboout the roots
     #in the other case the output is equivalent
     c_0 = c * f
     c_1 = b*f + c*d 
